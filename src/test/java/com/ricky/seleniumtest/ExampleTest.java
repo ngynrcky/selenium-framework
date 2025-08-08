20a8f4fd93d6432dc0585ddfbfda06218b62629f
Ricky Nguyen <ngynrcky@gmail.com>
2025-08-08 14:52:26 -0700

    feat: moving setUp and tearDown methods to BaseTest

--- src/test/java/com/ricky/seleniumtest/ExampleTest.java
+++ src/test/java/com/ricky/seleniumtest/ExampleTest.java
@@ -1,23 +1,11 @@
 package com.ricky.seleniumtest;
 
 import com.ricky.seleniumtest.pages.ExamplePage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest extends BaseTest {
-
-    @BeforeEach
-    public void setUpTest() {
-        setUp(); // from BaseTest
-    }
-
-    @AfterEach
-    public void tearDownTest() {
-        tearDown(); // from BaseTest
-    }
 
     @Test
     public void headingShouldBeCorrect() {
