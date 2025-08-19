440f673d4fc32bc90fa159599cc24c674708787f
Ricky Nguyen <ngynrcky@gmail.com>
2025-08-19 15:16:19 -0700

    feat: updated chromedriver to use headless mode if running through CI

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,10 +15,20 @@ public class BaseTest
     public void setUp(ITestResult result) {
         System.out.println("===== Starting test: " + result.getMethod().getMethodName() + " =====");
         System.out.println("[INFO] Initializing ChromeDriver...");
-        driver = new ChromeDriver();
-        System.out.println("[INFO] ChromeDriver initialized.");
+        ChromeOptions options = new ChromeOptions();
 
-        driver.manage().window().maximize();
+        // Detect if running in GitHub Actions
+        String ci = System.getenv("CI");
+        if (ci != null && ci.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new");   // use new headless mode
+            options.addArguments("--disable-gpu");   // required on some systems
+        }
+
+        // Always enforce a consistent viewport size
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        System.out.println("[INFO] ChromeDriver initialized.");
     }
 
     @AfterMethod
