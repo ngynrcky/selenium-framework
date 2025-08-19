87038f44db27846c9dffbd499347722cfb5b5b96
Ricky Nguyen <ngynrcky@gmail.com>
2025-08-19 14:40:40 -0700

    feat: updated api test to assert response time

--- src/test/java/tests/ExampleRestAssuredTest.java
+++ src/test/java/tests/ExampleRestAssuredTest.java
@@ -2,22 +2,34 @@ package tests;
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
 
 public class ExampleRestAssuredTest extends BaseApiTest{
     
     @Test
     public void exampleApiTest() {
         String url = "https://jsonplaceholder.typicode.com/todos/1";
+        String schemaPath = "schemas/jsonplaceholder/todos-schema.json";
 
-        get(url).
-        then().
-        statusCode(200).
-        body("userId", equalTo(1)).
-        body("id", equalTo(1)).
-        body("title", equalTo("delectus aut autem")).
-        body("completed", equalTo(false)).body(matchesJsonSchemaInClasspath("schemas/jsonplaceholder/todos-schema.json"));
+        Response response = get(url)
+            .then()
+                .statusCode(200)
+                .body("userId", equalTo(1))
+                .body("id", equalTo(1))
+                .body("title", equalTo("delectus aut autem"))
+                .body("completed", equalTo(false))
+                .body(matchesJsonSchemaInClasspath(schemaPath))
+            .extract()
+            .response();
+
+        long responseTime = response.getTime();
+        System.out.println("Response time: " + responseTime + " ms");
+        assertThat("Response time should be less than 1000 ms", responseTime, lessThan(1000L));
     }
 }
